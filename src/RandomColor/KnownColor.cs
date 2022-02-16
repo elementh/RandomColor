b3e2e33fdc88289058cce6b362b9793d88928221
Lucas Maximiliano Marino <hello@lucasmarino.me>
2022-02-16 09:36:02 +0100

    chore: update documentation for KnownColor

--- src/RandomColor/KnownColor.cs
+++ src/RandomColor/KnownColor.cs
@@ -1,12 +1,35 @@
 namespace RandomColor;
 
+/// <summary>
+/// Known color and it's ranges.
+/// </summary>
 public record KnownColor
 {
+    /// <summary>
+    /// Hue range.
+    /// </summary>
     public Range Hue { get; init; }
+    
+    /// <summary>
+    /// Lower bounds.
+    /// </summary>
     public Range[] LowerBounds { get; init; }
+    
+    /// <summary>
+    /// Saturation.
+    /// </summary>
     public Range Saturation { get; init; }
+    
+    /// <summary>
+    /// Brightness.
+    /// </summary>
     public Range Brightness { get; init; }
 
+    /// <summary>
+    /// Default constructor.
+    /// </summary>
+    /// <param name="hue">The hue range of the color.</param>
+    /// <param name="lowerBounds">The lower bounds of the color.</param>
     public KnownColor(Range hue, Range[] lowerBounds)
     {
         Hue = hue;
@@ -23,6 +46,11 @@ public record KnownColor
         Brightness = new Range(brightnessMin, brightnessMax);
     }
 
+    /// <summary>
+    /// Determines if the hue value passed to the function is inside the hue range of this color.
+    /// </summary>
+    /// <param name="hue"></param>
+    /// <returns>True if inside the range, false otherwise.</returns>
     public bool Includes(int hue)
     {
         return hue >= Hue.Start.Value && hue <= Hue.End.Value;
