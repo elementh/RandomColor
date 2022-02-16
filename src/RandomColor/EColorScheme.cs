7f90ae5eb28518339d3dfd97e4b6f2b4a25d5ee6
Lucas Maximiliano Marino <hello@lucasmarino.me>
2022-02-16 09:11:44 +0100

    feat: create ColorScheme enum

---
+++ src/RandomColor/EColorScheme.cs
@@ -0,0 +1,37 @@
+namespace RandomColor;
+
+public enum EColorScheme
+{
+    /// <summary>
+    /// Monochrome.
+    /// </summary>
+    Monochrome,
+    /// <summary>
+    /// Red.
+    /// </summary>
+    Red,
+    /// <summary>
+    /// Orange.
+    /// </summary>
+    Orange,
+    /// <summary>
+    /// Yellow.
+    /// </summary>
+    Yellow,
+    /// <summary>
+    /// Green.
+    /// </summary>
+    Green,
+    /// <summary>
+    /// Blue.
+    /// </summary>
+    Blue,
+    /// <summary>
+    /// Purple.
+    /// </summary>
+    Purple,
+    /// <summary>
+    /// Pink.
+    /// </summary>
+    Pink
+}
