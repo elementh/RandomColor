b961428565a72b67ade87051fbe0cc6e8a68e812
Lucas Maximiliano Marino <hello@lucasmarino.me>
2022-02-16 15:23:01 +0100

    feat: continue working on RandomColor

--- src/RandomColor/RandomColor.cs
+++ src/RandomColor/RandomColor.cs
@@ -5,32 +5,71 @@ namespace RandomColor;
 public static class RandomColor
 {
     private static readonly ColorLibrary ColorLibrary;
-    private static readonly Random Random;
+    private static Random _random;
+    private static bool _randomHasSeed;
 
     static RandomColor()
     {
         ColorLibrary = new ColorLibrary();
-        Random = new Random();
+        _random = new Random();
     }
 
-    public static Color Get(EColorScheme? scheme = default, ELuminosity? luminosity = default)
+    public static Color Get(EColorScheme? scheme = default, ELuminosity? luminosity = default, int? seed = default)
     {
-        scheme ??= GetRandomColorScheme();
-        luminosity ??= GetRandomLuminosity();
+        if (seed is not null)
+        {
+            _random = new Random(seed.Value);
+            _randomHasSeed = true;
+        }
+        else if (_randomHasSeed)
+        {
+            _random = new Random();
+            _randomHasSeed = false;
+        }
+
+        var hue = PickHue(scheme);
+        var saturation = PickSaturation(hue, luminosity);
+    }
+    private static int PickHue(EColorScheme? scheme)
+    {
+        if (scheme is null)
+        {
+            return _random.Next(0, 361);
+        }
+
+        return RandomWithin(ColorLibrary.GetColor(scheme)?.Hue ?? new Range(0, 361));
+    }
+
+    private static int PickSaturation(int hue, ELuminosity? eLuminosity)
+    {
+        var saturation = ColorLibrary.GetSaturationRange(hue);
+
+        if (eLuminosity is null)
+        {
+            return RandomWithin(new Range(0, 100));
+        }
         
-        return Color.Transparent;
+        return eLuminosity switch
+        {
+            ELuminosity.Bright => RandomWithin(new Range(55, saturation.Value.End)),
+            ELuminosity.Light => RandomWithin(new Range(saturation.Value.Start, 55)),
+            ELuminosity.Dark => RandomWithin(new Range(saturation.Value.End.Value - 10, saturation.Value.End.Value)),
+            _ => RandomWithin(saturation.Value)
+        };
     }
 
-    private static EColorScheme GetRandomColorScheme()
+    private static int RandomWithin(Range range)
     {
-        var values = Enum.GetValues<EColorScheme>();
+        if (range.Start.Value > range.End.Value)
+        {
+            range = new Range(range.End, range.Start);
+        }
 
-        return values[Random.Next(values.Length)];
-    }
-    private static ELuminosity GetRandomLuminosity()
-    {
-        var values = Enum.GetValues<ELuminosity>();
-
-        return values[Random.Next(values.Length)];
+        if (range.Start.Value == range.End.Value)
+        {
+            range = new Range(range.Start, range.End.Value + 1);
+        }
+        
+        return _random.Next(range.Start.Value, range.End.Value + 1);
     }
 }
